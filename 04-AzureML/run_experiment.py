46ed5e464c97e80906c0b116b361dbfab7e4c59a
Pablo Alvarez Doval <pablo.doval@gmail.com>
2018-10-19 18:17:05 +0200

    Added the AzureML 101 scenario.

--- 04-AzureML/run_experiment.py
+++ 04-AzureML/run_experiment.py
@@ -1,31 +1,34 @@
 import os
 import shutil
 from azureml.core import Workspace, Experiment
-from training.compute_target.vm_manager import VMManager
+from azureml.core.authentication import ServicePrincipalAuthentication
+from vm_manager import VMManager
 
 if __name__ == '__main__': 
-    #az login
-    #az account set --subscription <subscriptionId>
+    # Security for the RBAC principal
+    client_id = 'e0ae87de-e27b-433a-b7b5-017b0cd0808f'
+    client_secret = 'de90f973-5a74-4e1c-a3a1-fa8265ca8ccd'
+    tenant_id = '5c384fed-84cc-44a6-b34a-b060bf102a6e'
+    servicePrincipalAuth = ServicePrincipalAuthentication(tenant_id, client_id, client_secret)
 
+    # Compute Target
     compute_target_name = "fashionMNISTVM"
-    compute_size_vm = "Standard_NC6" # "Standard_D2_v2"
+    compute_size_vm = "Standard_NC6"
     
-    workspace = Workspace.from_config()
+    workspace = Workspace.from_config(auth=servicePrincipalAuth)
     compute_manager = VMManager(workspace)
     compute_target = compute_manager.get_or_create(compute_target_name, vm_size=compute_size_vm)
 
-    # print('Load data')
-    # data_store = workspace.get_default_datastore()
-    # data_store.upload(src_dir=config['train']['local_dataset_folder'], target_path=config['train']['remote_dataset_folder'], overwrite=True, show_progress=True)
-
     print('Prepare environment and code')
     script_folder = './training'
+    if (not os.path.exists(script_folder)):
+        os.makedirs(script_folder)    
     entry_script = 'train.py'
     shutil.copy(entry_script, script_folder)
     shutil.copy('model.py', script_folder)
 
     script_params = []
-    conda_libs = ['numpy', 'tensorflow', 'azureml-sdk[automl]']
+    conda_libs = ['numpy', 'azureml-sdk[automl]', 'tensorflow']
     experiment_config = compute_manager.get_script_config(script_folder, entry_script, script_params, compute_target, conda_packages=conda_libs)
 
     print('Run experiment')
---
+++ 04-AzureML/vm_manager.py
@@ -0,0 +1,83 @@
+from training.compute_target.base import BaseComputeTarget
+from azureml.core.compute import DsvmCompute, RemoteCompute
+from azureml.core.runconfig import RunConfiguration, DEFAULT_CPU_IMAGE
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core import ScriptRunConfig
+
+class VMManager(BaseComputeTarget):
+
+    def __init__(self, workspace):
+        self.__workspace = workspace
+
+    def get_or_create(self, compute_name, vm_size):
+        compute_target = self.__get_if_exist_compute_target(compute_name)
+
+        if (compute_target = None)
+            print('Creating a new compute target...')
+            vm_config = DsvmCompute.provisioning_configuration(vm_size=vm_size)
+            compute_target = DsvmCompute.create(self.__workspace, name=compute_name, provisioning_configuration=vm_config)
+            compute_target.wait_for_completion(show_output = True)
+
+        return compute_target
+        
+    def attach(self, compute_name, username, password, address):
+        compute_target = self.__get_if_exist_compute_target(compute_name)
+
+        if (compute_target = None)
+            compute_target = RemoteCompute.attach(self.__workspace, name=compute_name, username=params['username'], address=params['address'], password=params['password'])
+            compute_target.wait_for_completion(show_output=True)
+
+        return compute_target
+
+    def get_script_config(self, script_folder, entry_script, script_params, compute_target, data_path, channels=None, conda_packages=None, pip_packages=None):
+        run_config = self.__get_run_config(compute_target, data_path, channels, conda_packages, pip_packages)
+        script_run_config = ScriptRunConfig(source_directory=script_folder, script=entry_script, arguments=script_params, run_config=run_config)
+        return script_run_config
+
+    def __get_if_exist_compute_target(self, compute_name):
+        compute_target = None
+
+        if compute_name in self.__workspace.compute_targets():
+            target = self.__workspace.compute_targets()[compute_name]
+            if target and (type(target) is DsvmCompute or type(target) is RemoteCompute):
+                print('Found compute target: ' + compute_name)
+                compute_target = target
+
+        return compute_target
+        
+    def __get_run_config(self, compute_target, data_path, channels=None, conda_packages=None, pip_packages=None):
+        data_store = self.__workspace.get_default_datastore()
+
+        data_reference = DataReferenceConfiguration(datastore_name=data_store.name, 
+                   path_on_datastore=data_path, 
+                   mode='download', # download files from datastore to compute target
+                   overwrite=True)
+        
+        # Load the "cpu-dsvm.runconfig" file (created by the above attach operation) in memory
+        run_config = RunConfiguration(framework = "python")
+
+        # Set compute target to the Linux DSVM
+        run_config.target = compute_target.name
+
+        # Use Docker in the remote VM
+        run_config.environment.docker.enabled = False
+
+        # Ask system to provision a new one based on the conda_dependencies.yml file
+        run_config.environment.python.user_managed_dependencies = False
+
+        # Set the data reference of the run configuration
+        run_config.data_references = { data_store.name: data_reference }
+
+        # Prepare the Docker and conda environment automatically when used the first time.
+        run_config.prepare_environment = True
+
+        # specify dependencies obj
+        conda_dependencies = CondaDependencies.create(conda_packages=conda_packages, pip_packages=pip_packages)
+        if (channels):
+            for channel in channels:
+                conda_dependencies.add_channel(channel)
+
+        run_config.environment.python.conda_dependencies = conda_dependencies
+
+        return run_config
+
