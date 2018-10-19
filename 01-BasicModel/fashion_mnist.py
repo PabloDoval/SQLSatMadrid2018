ae66457676d67933ebd4f96381959c23d0eb6efc
Pablo Alvarez Doval <pablo.doval@gmail.com>
2018-10-19 12:53:36 +0200

    Updated notebook, and improved code on fashion:msnist.py.

--- 01-BasicModel/fashion_mnist.py
+++ 01-BasicModel/fashion_mnist.py
@@ -30,76 +30,103 @@ def plot_value_array(i, predictions_array, true_label):
   thisplot = plt.bar(range(10), predictions_array, color="#777777")
   plt.ylim([0, 1]) 
   predicted_label = np.argmax(predictions_array)
- 
+
   thisplot[predicted_label].set_color('red')
   thisplot[true_label].set_color('blue')
 
+def display_images(train_images, train_labels, class_names, num_images=25):
+    plt.figure(figsize=(10,10))
+    for i in range(num_images):
+        plt.subplot(5,5,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i]])
+    plt.show()
 
-# Load fashion mnist dataset
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+def plot_images_and_predictions(predictions, test_images, test_labels, num_rows=5, num_cols=3):
+    # Plot the first X test images, their predicted label, and the true label
+    # Color correct predictions in blue, incorrect predictions in red
+    num_images = num_rows*num_cols
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2*num_cols, 2*i+1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(num_rows, 2*num_cols, 2*i+2)
+        plot_value_array(i, predictions, test_labels)
+    plt.show()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+def load_data():
+    # Load fashion mnist dataset
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    return train_images, train_labels, test_images, test_labels, class_names
 
-# Explore data
-print(train_images.shape)
-print(train_labels)
-print(test_images.shape)
+def preprocess_data(train_images, test_images):
+    # Preprocess the data
+    # The data must be preprocessed before training the network. 
+    # The pixel values fall in the range of 0 to 255
+    # We scale these values to a range of 0 to 1 before feeding to the neural network model.
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+    return train_images, test_images
 
-# Preprocess the data
-# The data must be preprocessed before training the network. 
-# The pixel values fall in the range of 0 to 255
-# We scale these values to a range of 0 to 1 before feeding to the neural network model.
+def get_model_definition():
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    return model
 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+def train_model(model, train_images, train_labels, epochs=5):
+    # Compile model
+    model.compile(optimizer=tf.train.AdamOptimizer(), 
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
 
-print('Display the first 25 images')
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+    # Train model
+    model.fit(train_images, train_labels, epochs=epochs)    
 
-# Build model
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+def evaluate(model, test_images, test_labels):   
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('Test accuracy:', test_acc)
 
-# Compile model
-model.compile(optimizer=tf.train.AdamOptimizer(), 
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+def predict(model, test_images, test_labels, class_names):
+    predictions = model.predict(test_images)
 
-# Train model
-model.fit(train_images, train_labels, epochs=5)
+    print('Predictions for first image:')
+    print(predictions[0])
+    print('Category predicted for first image: ' + class_names[np.argmax(predictions[0])])
+    print('Real category for first image: ' + class_names[test_labels[0]])
 
-# Evaluate accuracy
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
+    plot_images_and_predictions(predictions, test_images, test_labels)
 
-predictions = model.predict(test_images)
+if __name__ == '__main__': 
+    # Load data
+    train_images, train_labels, test_images, test_labels, class_names = load_data()
 
-print('Predictions for first image:')
-print(predictions[0])
-print('Category predicted for first image: ' + str(np.argmax(predictions[0])))
-print('Real category for first image: ' + str(test_labels[0]))
+    # Explore data
+    print(train_images.shape)
+    print(train_labels)
+    print(test_images.shape)
+    display_images(train_images, train_labels, class_names)
 
-# Plot the first X test images, their predicted label, and the true label
-# Color correct predictions in blue, incorrect predictions in red
-num_rows = 5
-num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-    plt.subplot(num_rows, 2*num_cols, 2*i+1)
-    plot_image(i, predictions, test_labels, test_images)
-    plt.subplot(num_rows, 2*num_cols, 2*i+2)
-    plot_value_array(i, predictions, test_labels)
-plt.show()
+    # Preprocess data
+    train_images, test_images = preprocess_data(train_images, test_images)
+
+    # Build model
+    model = get_model_definition()
+
+    # Train model
+    train_model(model, train_images, train_labels)
+
+    # Evaluate accuracy
+    evaluate(model, test_images, test_labels)
+
+    # Predict test images
+    predict(model, test_images, test_labels, class_names)
+
+
